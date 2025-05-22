4387c50b2a56e05b8973614e79e17b14a7801507
jhwon48 <jhwon48@gmail.com>
2025-05-22 17:59:03 +0900

    5 / 22

--- react02-board1-without-db/src/main.jsx
+++ react02-board1-without-db/src/main.jsx
@@ -1,10 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } form 'react-router'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
 )
